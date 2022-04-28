23d3b581664800bd6e105c3dd685ba9f4aa00941
ali <mdali2016.227@gmail.com>
2022-04-28 14:02:40 +0600

    [Change] digit cards segmented to top and bottom portion

--- src/app.js
+++ src/app.js
@@ -1,19 +1,36 @@
+let digits = [0,0,0,0,0,0]
+let prevDigits = [0,0,0,0,0,0]
+
 document.addEventListener('DOMContentLoaded', () => {
     let app = document.getElementById('app')
-    app.innerHTML = clock(12, 12, 12)
+    app.innerHTML = clock(0, 0, 0)
+    runClock()
 })
 
-function clock(hour, minute, second) {
+function runClock() {
+    prevDigits = digits
+    digits = clockDigits()
+    digits.map((digit, index) => {
+        if (digit !== prevDigits[index]) {
+            changeDigit(index)
+        }
+    })
 
+    setTimeout(()=>{
+        runClock()
+    }, 333)
+}
+
+function clock(hour, minute, second) {
     let hourSegment = clockSegment('HOURS', cards(hour))
     let minuteSegment = clockSegment('MINUTES', cards(minute))
     let secondSegment = clockSegment('SECONDS', cards(second))
 
     return `
         <div class="clock">
-            <div class="clock__hour clock__segment">${hourSegment}</div>
-            <div class="clock__minute clock__segment">${minuteSegment}</div>
-            <div class="clock__second clock__segment">${secondSegment}</div>
+            <div class="clock__segment">${hourSegment}</div>
+            <div class="clock__segment">${minuteSegment}</div>
+            <div class="clock__segment">${secondSegment}</div>
         </div>
     `
 }
@@ -34,12 +51,41 @@ function cards(number) {
         : [card(Math.floor(number/10)), card(number%10)]
 }
 
-function card(digit) {
+function card(digit, id='card') {
     return `
-        <div class="card">
+        <div class="card" id="${id}">
             <div class="card__digit">${digit}</div>
-            <div class="card__top"></div>
-            <div class="card__bottom"></div>
+            <div class="card__top">
+                <div class="card__top__text card__top--flip">${digit}</div>
+                <div class="card__top__text">${digit}</div>
+            </div>
+            <div class="card__bottom">
+                <div class="card__bottom__text card__bottom--flip">${digit}</div>
+                <div class="card__bottom__text">${digit}</div>
+            </div>
         </div>
     `
+}
+
+function clockDigits(){
+    let date = new Date()
+
+    return [
+        Math.floor(date.getHours()/10),
+        date.getHours()%10,
+        Math.floor(date.getMinutes()/10),
+        date.getMinutes()%10,
+        Math.floor(date.getSeconds()/10),
+        date.getSeconds()%10,
+    ]
+}
+
+function changeDigit(index) {
+    let newCard = card(digits[index], 'new_card')
+    let cards = document.querySelectorAll('.card')
+    cards[index].querySelector('.card__top').innerText = digits[index]
+    cards[index].querySelector('.card__digit').innerText = digits[index]
+    cards[index].querySelector('.card__bottom__text').innerText = digits[index]
+    let cardTop = cards[index].querySelector('.card__top')
+    // cardTop.style.transform = 'rotateX(90deg)'
 }
